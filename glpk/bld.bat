116af80c534cfbd3bc5c37c403235b9a6cb48a60
Joshua Arnott <joshua.arnott@atkinsglobal.com>
2016-02-14 01:13:06 +0000

    Use copy instead of cp in glpk/bld.bat

--- glpk/bld.bat
+++ glpk/bld.bat
@@ -1,5 +1,5 @@
-cp src\glpk.h %LIBRARY_INC%\glpk.h
-cp w%ARCH%\*.lib %LIBRARY_LIB%
-cp w%ARCH%\glpk_4_57.lib %LIBRARY_LIB%\glpk.lib
-cp w%ARCH%\*.dll %LIBRARY_BIN%
-cp w%ARCH%\*.exe %LIBRARY_BIN%
+copy src\glpk.h %LIBRARY_INC%\glpk.h
+copy "w%ARCH%\\*.lib" %LIBRARY_LIB%
+copy "w%ARCH%\\glpk_4_57.lib" %LIBRARY_LIB%\glpk.lib
+copy "w%ARCH%\\*.dll" %LIBRARY_BIN%
+copy "w%ARCH%\\*.exe" %LIBRARY_BIN%
